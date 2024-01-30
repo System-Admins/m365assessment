8f85009d4fc4fc4a8c4a22cc92b482f699478a57
“Alex <ath@systemadmins.com>
2024-01-30 18:13:16 +0100

    add guest group

---
+++ src/public/entraid/identity/groups/Invoke-ReviewEntraGuestDynamicGroup.ps1
@@ -0,0 +1,43 @@
+function Invoke-ReviewEntraGuestDynamicGroup
+{
+    <#
+    .SYNOPSIS
+        Review that a dynamic group for guest users is created.
+    .DESCRIPTION
+        Return a list of dynamic groups that contain guest users.
+    .EXAMPLE
+        Invoke-ReviewEntraGuestDynamicGroup;
+    #>
+
+    [CmdletBinding()]
+    Param
+    (
+    )
+
+    BEGIN
+    {
+        # Get all dynamic groups.
+        $groups = Get-MgGroup -Filter "groupTypes/any(c:c eq 'DynamicMembership')" -All;
+
+        # Results to store groups.
+        $results = New-Object System.Collections.ArrayList;
+    }
+    PROCESS
+    {
+        # Foreach dynamic group.
+        foreach ($group in $groups)
+        {
+            # If group contains guest users.
+            if ($group.MembershipRule -eq '(user.userType -eq "Guest")')
+            {
+                # Add group to results.
+                $results.Add($group) | Out-Null;
+            }
+        }
+    }
+    END
+    {
+        # Return results.
+        return $results
+    }
+}
